3be4cd6b25d4255443ae5820f5358a22c37de31a
Dustin Bachrach <ahdustin@gmail.com>
2013-11-13 23:34:16 -0600

    Remove loadlayout from countdown.js

--- js/distr/countdown/page/countdown.js
+++ js/distr/countdown/page/countdown.js
@@ -5,27 +5,27 @@ define (["jquery", "underscore", "tri/core/helpers"], function($, _, helpers) {
 		this.data = data;
 		this.time = helpers.ParseTime(data.time);
 		this.trial = trial;
+		this.bindable = ["title", "content"];
 	}
 	CountdownPage.prototype.show = function() {
 		console.log("Showing CountdownPage");
 
 		var base = this;
-		// TODO: Remove use of load layout
-		helpers.LoadLayout("countdown", ["title", "content"], this.data, function () {
-			var count = base.time;
-			function countdown(val, completion) {
-				console.log("countdown with " + val);
-				if (val == 0) {
-					completion();
-				}
-				else {
-					$("#countdown").text(val);
-					_.delay(countdown, 1000, val - 1, completion);
-				}
+		
+		function countdown(val, completion) {
+			console.log("countdown with " + val);
+			if (val == 0) {
+				completion();
 			}
-			countdown(count / 1000, function () {
-				base.end();
-			});
+			else {
+				$("#countdown").text(val);
+				_.delay(countdown, 1000, val - 1, completion);
+			}
+		}
+
+		var count = this.time / 1000;
+		countdown(count, function () {
+			base.end();
 		});
 	};
 	CountdownPage.prototype.end = function () {
